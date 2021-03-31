d350c3e28c2e3ff74c54b5694a801a60698e9029
fernandaalve <78861070+fernandaalve@users.noreply.github.com>
2021-03-31 16:05:30 -0300

    configurei o código

--- AC1.ino
+++ AC1.ino
@@ -1,66 +1,72 @@
-//variaveis da led
-const int vermelho = 5;
-const int verde = 6;
-const int azul = 7
+// variaveis da led
+const  int vermelho = 5 ;
+const  int verde = 6 ;
+const  int azul = 7;
 
-bool estadoLedVermelho = false;
+bool estadoLedVermelho = false ; 
+bool estadoLedVerde = false;
+bool estadoLedAzul = false;
 
-const int botao1 = 2;
-unsigned long lastDebounceTime1 = 0;
-const int botaoDelay = 100;
+const  int botao1 = 2 ;
+unsigned  long lastDebounceTime1 = 0 ;
+const  int botaoDelay = 100 ;
 
-void setup()
+void  setup()
 {
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
+  pinMode (A0, INPUT); 
+  pinMode (A1, INPUT);
+
+  pinMode (vermelho, OUTPUT);
+  pinMode (verde, OUTPUT);
+  pinMode (azul,OUTPUT);
   
-  pinMode(vermelho, OUTPUT);
-  pinMode(verde, OUTPUT);
-  pinMode(azul, saida);
+ 
+  Serial.begin ( 9600 ); //começar o Sereal
   
-  Serial.begin(9600);
-	
-  Serial.println("AC1 - Meu Primeiro Projeto 2021");
-  Serial.println("                           V1.0");
-  Serial.println("Grupo: NOME AQUI               ");
+  Serial.println ("começar"); 
+  Serial.println ( " AC1 - Meu Primeiro Projeto 2021 " );
+  Serial.println ( "                            V1.0 " );
+  Serial.println ( " Grupo: Lion Eye              " );
 }
 
-void loop()
+void  loop()
 {
-  if((millis() - lastDebounceTime1) > botaoDelay && digitalRead(botao1)){
-  	Serial.println("botao 1 apertado");
-    ledVermelho();
-  	lastDebounceTime1 = millis();
+  if (( millis () - lastDebounceTime1)> botaoDelay && digitalRead (botao1)) {
+  	Serial. println ( " botao 1 apertado " );
+    ledVermelho (true);
+  	lastDebounceTime1 = millis ();
   }
   
-  if(getTemperatura() > 30){
-    ledAzul(true);
-  }else{
-  	ledAzul(falso); 
+  if ( getTemperatura ()> 15 ) {
+    ledAzul ( true );
+  } else {
+  	ledAzul (false);
   }
   	
-  delay(10);
+  delay( 10 );
+}
+ 
+ void  ledVermelho (bool estado) { 
+estadoLedVermelho =!estadoLedVermelho;
+  digitalWrite (vermelho, estadoLedVermelho);
 }
 
-void ledVermelho(){
-  estadoLedVermelho = !estadoLedVermelho;
-  digitalWrite(vermelho,estadoLedVermelho);
+void  ledVerde (bool estado) {
+  estadoLedVerde =!estadoLedVerde;
+  digitalWrite (verde, estadoLedVerde);
 }
-void ledVerde(){
- //precisa fazer  
+ void  ledAzul (bool estado) {
+  estadoLedAzul =!estadoLedAzul;
+  digitalWrite (azul, estadoLedAzul);
 }
-void ledAzul(bool estado){
-	digitalWrite(azul,estado);
-}
-
-int getTemperatura(){
+int  getTemperatura () {
   	int temperaturaC;
-	temperaturaC = map(((analogRead(A0) - 20) * 3.04), 0, 1023, -40, 125);
+	temperaturaC = map ((( analogRead (A0) - 20 ) * 3,04 ), 0 , 1023 , - 40 , 125 );
   	return temperaturaC;
 } 
 
-int getLuminosidade(){
+int  getLuminosidade () {
   	int luminosidade;
-	luminosidade = map(analogRead(A1), 6, 619, -3, 10);
+	luminosidade = map ( analogRead (A1), 6 , 619 , - 3 , 10 );
   	return luminosidade;
-}
+}mpe
